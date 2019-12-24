389b2a6ac6dd45d582c141576bad7f1573473976
krasnia2 <krasnia2@gmail.com>
2019-12-24 17:09:51 -0500

    adding eureka client

---
+++ sample-client/src/main/java/com/krasniqi/sampleclient/controller/SampleController.java
@@ -0,0 +1,19 @@
+package com.krasniqi.sampleclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class SampleController {
+
+    @Value("${service.instance.name}")
+    private String instance;
+
+    @GetMapping
+    public String message() {
+        return "Hello from " + instance;
+    }
+}
