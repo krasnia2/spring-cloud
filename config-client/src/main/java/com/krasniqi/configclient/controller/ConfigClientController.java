8e939c97f9bd105d472fb304ec54655fd9eafdda
krasnia2 <krasnia2@gmail.com>
2019-12-25 12:56:59 -0500

    Adding config client

---
+++ config-client/src/main/java/com/krasniqi/configclient/controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package com.krasniqi.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ConfigClientController {
+
+    @Value("${some.property}")
+    private String someProperty;
+
+
+    @GetMapping
+    public String ping(){
+        return "Config client up. Injected property from config server: " + someProperty;
+    }
+}
